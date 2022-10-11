99e059027fe54aeb078f854bc07897b628255d5e
Willeak <willeak62700@gmail.com>
2022-10-11 11:15:45 +0200

    correction format dateDefault backend

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,6 +1,7 @@
 //appel des middlewares
 const mongoose = require("mongoose");
-
+var moment = require("moment");
+var momentDate = moment().format("YYYYMMDDHHMMssSSSSSS");
 // Création d'un schema de donnée Sauce pour un stockage dans la BD
 const postSchema = mongoose.Schema({
   userImg: { type: String, required: true },
@@ -9,7 +10,7 @@ const postSchema = mongoose.Schema({
   imageUrl: { type: String, default: "" },
   description: { type: String, required: true },
   date: { type: String, required: true },
-  dateDefault: { type: String, default: Date.now() },
+  dateDefault: { type: String, default: momentDate },
   likes: { type: Number, default: 0 },
   usersLiked: { type: [String], default: [] },
 });
