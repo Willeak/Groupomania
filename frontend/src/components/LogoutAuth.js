9c91629fbf6968ae1f9d84012bbc7f4f01d33209
Willeak <willeak62700@gmail.com>
2022-10-15 04:25:45 +0200

    commentaire route private

--- frontend/src/components/LogoutAuth.js
+++ frontend/src/components/LogoutAuth.js
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 function LogoutAuth() {
+      // appel du token stocké si existant
       let token = sessionStorage.getItem('token');
       const isLogin = token;
       // si un token est présent acces a  la route
--- frontend/src/components/RequireAuth.js
+++ frontend/src/components/RequireAuth.js
@@ -2,34 +2,24 @@ import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import Jwt_Decode from 'jwt-decode';
 
 function RequireAuth() {
+      //token stoké
       let token = sessionStorage.getItem('token');
       const isLogin = token;
-
-      const initialstate = {
-            user: null,
-      };
-
-      // const authed = JSON.parse(localStorage.getItem('authed'));
-      // const istoken = authed.token;
-
+      // appel de use route
       let navigate = useNavigate();
-
+      //si le token existe alors
       if (isLogin) {
+            //decoder le token
             const jwt_Token_decoded = Jwt_Decode(isLogin);
-            console.log('1:' + jwt_Token_decoded.exp * 1000);
-
-            console.log('3: ' + Date.now());
+            //si le token est inferieur a la date alors clear les stockage et renvoyer vers login
             if (jwt_Token_decoded.exp * 1000 < Date.now()) {
                   localStorage.clear(); // this runs only when I refresh the page or reload on route change it dosent work
                   sessionStorage.clear();
                   <Navigate to="/login" />;
-
                   navigate('/login');
             }
       }
-
-      console.log('Is User Login?', isLogin);
-      // si un token est présent acces a  la route
+      // si un token est présent acces a la route
       return isLogin ? <Outlet /> : <Navigate to="/login" />;
 }
 
